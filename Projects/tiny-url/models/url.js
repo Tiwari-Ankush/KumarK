28ded12dfe1e5212f484aa0203b0eaeabd42a2d5
Ankush <ankushgt25@gmail.com>
2024-03-09 14:50:57 +0530

    DEV

---
+++ Projects/tiny-url/models/url.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema({
+    shortId:{
+        type:String,
+        reqire : true,
+        unique:true,
+    },
+    redirectUrl:{
+        type : String,
+        require : true,
+    
+    },
+    visitHistory : [{timestamp : {type :Number}}],
+},
+{timestamps :true}
+);
+
+
+const URL  = mangoose.model('url',urlSchema)
+
+module.exports = URL;
