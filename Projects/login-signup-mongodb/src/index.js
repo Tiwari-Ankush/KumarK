bef32eb6d1630bf5e298bdeb157ee19c1d67c920
Ankush <ankushgt25@gmail.com>
2024-03-09 21:55:55 +0530

    login signup

--- Projects/login-signup-mongodb/src/index.js
+++ Projects/login-signup-mongodb/src/index.js
@@ -1,99 +1,55 @@
-const express = require("express")
-const path = require("path")
-const app = express()
-// const hbs = require("hbs")
-const LogInCollection = require("./mongo")
-const port = process.env.PORT || 3000
-app.use(express.json())
+const express = require("express");
+const path = require("path");
+const app = express();
+const LogInCollection = require("./mongo");
+const port = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const tempelatePath = path.join(__dirname, '../templates')
-const publicPath = path.join(__dirname, '../public')
-console.log(publicPath);
+const templatePath = path.join(__dirname, '../templates');
+const publicPath = path.join(__dirname, '../public');
 
-app.set('view engine', 'hbs')
-app.set('views', tempelatePath)
-app.use(express.static(publicPath))
-
-
-// hbs.registerPartials(partialPath)
-
+app.set('view engine', 'hbs');
+app.set('views', templatePath);
+app.use(express.static(publicPath));
 
 app.get('/signup', (req, res) => {
-    res.render('signup')
-})
+    res.render('signup');
+});
+
 app.get('/', (req, res) => {
-    res.render('login')
-})
-
-
-
-// app.get('/home', (req, res) => {
-//     res.render('home')
-// })
+    res.render('login');
+});
 
 app.post('/signup', async (req, res) => {
-    
-    // const data = new LogInCollection({
-    //     name: req.body.name,
-    //     password: req.body.password
-    // })
-    // await data.save()
-
-    const data = {
-        name: req.body.name,
-        password: req.body.password
+    try {
+        const data = new LogInCollection({
+            name: req.body.name,
+            password: req.body.password
+        });
+        await data.save();
+        res.status(201).render("home", { naming: req.body.name });
+    } catch (error) {
+        console.error(error);
+        res.send("Error signing up");
     }
-
-    const checking = await LogInCollection.findOne({ name: req.body.name })
-
-   try{
-    if (checking.name === req.body.name && checking.password===req.body.password) {
-        res.send("user details already exists")
-    }
-    else{
-        await LogInCollection.insertMany([data])
-    }
-   }
-   catch{
-    res.send("wrong inputs")
-   }
-
-    res.status(201).render("home", {
-        naming: req.body.name
-    })
-})
-
+});
 
 app.post('/login', async (req, res) => {
-
     try {
-        const check = await LogInCollection.findOne({ name: req.body.name })
-
-        if (check.password === req.body.password) {
-            res.status(201).render("home", { naming: `${req.body.password}+${req.body.name}` })
+        const user = await LogInCollection.findOne({ name: req.body.name, password: req.body.password });
+        if (user) {
+            res.status(201).render("home", { naming: req.body.name });
+        } else {
+            res.send("Incorrect username or password");
         }
-
-        else {
-            res.send("incorrect password")
-        }
-
-
-    } 
-    
-    catch (e) {
-
-        res.send("wrong details")
-        
-
+    } catch (error) {
+        console.error(error);
+        res.send("Error logging in");
     }
-
-
-})
-
-
+});
 
 app.listen(port, () => {
-    console.log('port connected');
-})
+    console.log(`Server is running at port ${port}`);
+});
--- Projects/login-signup-mongodb/src/mongo.js
+++ Projects/login-signup-mongodb/src/mongo.js
@@ -1,24 +1,25 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/LoginFormPractice")
-.then(()=>{
-    console.log('mongoose connected');
-})
-.catch((e)=>{
-    console.log('failed');
-})
+mongoose.connect("mongodb://localhost:27017/LoginFormPractice", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('MongoDB connected');
+}).catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
+});
 
-const logInSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const logInSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    password:{
-        type:String,
-        required:true
+    password: {
+        type: String,
+        required: true
     }
-})
+});
 
-const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+const LogInCollection = mongoose.model('LogInCollection', logInSchema);
 
-module.exports=LogInCollection
+module.exports = LogInCollection;
