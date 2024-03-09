429743cfb5eef50a89b358b9c0e59fb4d7b7b0a0
Ankush <ankushgt25@gmail.com>
2024-03-09 15:18:27 +0530

    DEV

--- Projects/tiny-url/index.js
+++ Projects/tiny-url/index.js
@@ -6,6 +6,12 @@ const{connectToMongoDB} = require("./connect")
 const app = express();
 const PORT = 3002;
 
+
+connectToMongoDB('mongodb://localhost:27017/short-url').then(() =>
+console.log("mongodb conneccted")
+);
+
+
 app.use("/url",urlRoute);
 
 app.listen(PORT, () => console.log("Server is running at port ${PORT}"));
