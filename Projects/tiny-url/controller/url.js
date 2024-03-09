30a8475de0ab0cc9ac41aa36d47bd216392a48d3
Ankush <ankushgt25@gmail.com>
2024-03-09 21:01:22 +0530

    tiny-url-proj added

--- Projects/tiny-url/controller/url.js
+++ Projects/tiny-url/controller/url.js
@@ -1,19 +1,20 @@
-const { nanoid } = require("nanoid");
-const URL = require('../models/url');
 
-async function handleGenerateNewShortURL(req, res) {
-    const body = req.body; // Moved this line up to prevent undefined error
-    if (!body.url) return res.status(400).json({ error: "url is required" });
+const {nanoid} = require("nanoid");
+const URL = require('../models/url')
+
+async function handleGenerateNewShortURL(req,res){
+    if(!body.url) return res.status(400).json({error: "url is required"});
+    const body = req.body;
 
     const shortId = nanoid(10);
     await URL.create({
-        shortId: shortId,
+        shortid : shortId,
         redirectURL: body.url,
         visitHistory: []
-    });
-    return res.json({ id: shortId });
+    })
+    return res.json({id: shortId})
 }
 
 module.exports = {
     handleGenerateNewShortURL,
-};
+}
